0a9c38c418f80cb4a8ad8f09a4ce73a4eb6a2544
G1ZMODRAG0N <elliottchinton@gmail.com>
2026-05-22 07:35:09 -0400

    updated initialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,23 @@
 #![recursion_limit = "10000"]
+use std::{thread::spawn, time::Duration};
+
 use eldenring::util::system::{SystemInitError, wait_for_system_init};
-use fromsoftware_shared::{InstanceError, Program};
+use fromsoftware_shared::{
+    F32Vector4, FromStatic, InstanceError, SharedTaskImpExt, program::Program,
+};
 use thiserror::Error;
 
 // use
 mod cam;
 mod config;
 mod rva;
+
+pub fn init() {
+    spawn(move || {
+        let program = Program::current();
+        wait_for_system_init(&program, Duration::MAX).unwrap();
+    });
+}
 
 pub unsafe extern "C" fn dll_main(_hmodule: usize, reason: u32) -> bool {
     if reason == 1 {
@@ -15,6 +26,7 @@ pub unsafe extern "C" fn dll_main(_hmodule: usize, reason: u32) -> bool {
             let program = Program::current();
             cam::hook(&program).expect("Could not apply camera hook");
         });
+        init();
     }
     true
 }
---
+++ target/debug/deps/OpenCam-d9ce4e5ce8ba36db.d
@@ -0,0 +1,17 @@
+E:\Modding\third-party-sources\OpenCam\target\debug\deps\OpenCam-d9ce4e5ce8ba36db.d: src\lib.rs src\cam.rs src\cam\auto_transition.rs src\cam\cam_properties.rs src\cam\follow.rs src\cam\lock_on_hud.rs src\cam\spectate.rs src\cam\top_down.rs src\config.rs src\rva.rs src\rva\bundle.rs src\rva\rva_jp.rs src\rva\rva_ww.rs
+
+E:\Modding\third-party-sources\OpenCam\target\debug\deps\libOpenCam-d9ce4e5ce8ba36db.rmeta: src\lib.rs src\cam.rs src\cam\auto_transition.rs src\cam\cam_properties.rs src\cam\follow.rs src\cam\lock_on_hud.rs src\cam\spectate.rs src\cam\top_down.rs src\config.rs src\rva.rs src\rva\bundle.rs src\rva\rva_jp.rs src\rva\rva_ww.rs
+
+src\lib.rs:
+src\cam.rs:
+src\cam\auto_transition.rs:
+src\cam\cam_properties.rs:
+src\cam\follow.rs:
+src\cam\lock_on_hud.rs:
+src\cam\spectate.rs:
+src\cam\top_down.rs:
+src\config.rs:
+src\rva.rs:
+src\rva\bundle.rs:
+src\rva\rva_jp.rs:
+src\rva\rva_ww.rs:
